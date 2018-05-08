1dfb21fa90b00e529f53432a7666372a8d13bb95
Nikhil Arora <narora200@gmail.com>
2018-05-08 10:20:43 +0530

    a simple mutation done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
 
 // 2
 let links = [{
@@ -21,23 +8,29 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    // 2
     feed: () => links,
   },
-  // 3
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
+  Mutation: {
+    // 2
+    post: (root, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
   }
 }
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
